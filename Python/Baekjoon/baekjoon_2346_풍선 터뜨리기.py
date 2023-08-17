ebc6ffa735b8a8b3ccccf52505dc586dc4f3cfc4
jxxn92 <futjxxn@naver.com>
2023-08-17 16:03:08 +0900

    Baekjoon 2346 Deque *rotate 함수, enumerate 함수

--- Python/Baekjoon/baekjoon_2346_풍선 터뜨리기.py
+++ Python/Baekjoon/baekjoon_2346_풍선 터뜨리기.py
@@ -20,3 +20,4 @@ print(*ans)
 
 #TODO Python Deque 모듈에 rotate라는 함수가 존재 
 #TODO rotate 함수는 .rotate() 꼴로 사용 되는데 양수면 오른쪽으로 회전 음수면 왼쪽으로 회전 알아놓으면 유용하게 쓰일 수 있을 것 같다.
+#TODO enumerate 라는 함수도 존재하는데 값과 인덱스를 함께 출력하고 싶을때 사용하면 된다.
