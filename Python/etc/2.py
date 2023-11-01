3af53514d531eebad5e185b31e3062be1d6871b7
jxxn92 <futjxxn@naver.com>
2023-11-01 19:01:23 +0900

    <I>

--- Python/etc/2.py
+++ Python/etc/2.py
@@ -1,26 +1,34 @@
-import copy
-
 lst = input().rstrip().split()
 lst2 = [] 
 lst3 = []
 
-# if lst[0] == "D":
-#     lst2.append("Tx")
-#     for i in range(1, len(lst)):
-#         if lst[i] == "7E" or lst[i] == "1B":
-#             lst2.append("1B")
-#             lst2.append(lst[i])
-#         else:
-#             lst2.append(lst[i])
+next_val = False
+
+if lst[0] == "D":
+    lst2.append("Tx")
+    for i in range(1, len(lst)):
+        if lst[i] == "7E" or lst[i] == "1B":
+            lst2.append("1B")
+            lst2.append(lst[i])
+        else:
+            lst2.append(lst[i])
 if lst[0] == "Rx":
     lst2.append("D")
     for i in range(1,len(lst)):
         lst2.append(lst[i])
         if (lst[i-1] == "1B" and lst[i] == "7E"):
             lst2.pop(-2)
-    print(lst2)
-# D 7E 7C BA 78 1B 1B 1B 1B B6 7E 
-
-# print(" ".join(lst2))
+    for i in range(len(lst2)):
+        if(next_val == True):
+            next_val = False
+            continue
+        lst3.append(lst2[i])
+        if(lst2[i] == "1B"):
+            next_val = True
+        
+if(lst[0] == "D"):
+    print(" ".join(lst2))
+else:
+    print(" ".join(lst3))
 
 
